5ef00eddebcb09062b967f8be6c613b9be8ed976
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-09-08 18:12:30 +0200

    Converter added

--- XNJNUA/MainPage.xaml.cs
+++ XNJNUA/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using XNJNUA.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,33 @@ namespace XNJNUA
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public List<TodoItem> Todos { get; set; } = new List<TodoItem>()
+        {
+            new TodoItem()
+            {
+                Id = 1,
+                Title = "Todo 1",
+                Description = "Description for Todo 1",
+                Priority = Priority.Normal,
+                IsDone = true,
+                Deadline = DateTimeOffset.Now + TimeSpan.FromDays(1)
+            },
+
+            new TodoItem()
+            {
+                Id = 2,
+                Title = "Todo 2",
+                Description = "Description for Todo 2",
+                Priority = Priority.High,
+                IsDone = false,
+                Deadline = new DateTime(2022, 12, 09, 12, 00, 00, 00)
+            }
+        };
+
         public MainPage()
         {
             this.InitializeComponent();
+            DataContext = this;
         }
     }
 }
