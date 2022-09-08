752c179a7e6b8f99743d40c5aabd7dc9a51f7f54
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-09-08 17:50:14 +0200

    Add project files.

---
+++ XNJNUA/Models/Priority.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XNJNUA.Models
+{
+    public enum Priority
+    {
+        High,
+        Normal,
+        Low
+    }
+}
