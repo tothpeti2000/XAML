29b391f764c634d46638fe82b8ae6d6d8dae8e08
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-09-11 21:07:51 +0200

    Priority colors added

---
+++ XNJNUA/Converters/PriorityToBackgroundConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using XNJNUA.Models;
+
+namespace XNJNUA.Converters
+{
+    public class PriorityToBackgroundConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var priority = (Priority)value;
+
+            switch (priority)
+            {
+                case Priority.High: return "Red";
+                case Priority.Normal: return "Orange";
+                case Priority.Low: return "Green";
+                default: return "White";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
